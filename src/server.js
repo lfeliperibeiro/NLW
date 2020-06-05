7307611dfac36ebc4e550b31eb46520054986f52
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-04 21:43:36 -0300

    Update
    Criação do Servidor, Utilização de Express, refatoração do código utilizando template engine Nunjucks e organização das pastas

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static('public'))
+
+// Utilizando template engine
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+server.get('/', (req, res) =>{
+    return res.render('index.html', {title: 'Um título'})
+})
+
+server.get('/create-point', (req, res) =>{
+    return res.render('create-point.html')
+})
+
+server.get('/search', (req, res) =>{
+    return res.render('search-results.html')
+})
+
+server.listen(3000)
